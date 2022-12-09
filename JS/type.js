7ba39794c899607d6e3b024f65cf978915dbdd35
Rico Susanto <109891252+MustankLee@users.noreply.github.com>
2022-12-09 10:08:39 +0700

    Update type.js

--- JS/type.js
+++ JS/type.js
@@ -2,7 +2,7 @@
 // the code of typing animation is from: http://www.mattboldt.com/demos/typed-js/
 // the code of typing animation is from: https://github.com/mattboldt/typed.js/
 var typing_view = new Typed(".typing", {
-    strings: ["Rico Susanto","a Programmer", "a Frontend Developer"],
+    strings: ["Rico Susanto","a Programmer", "a Developer"],
     typeSpeed: 200,
     backSpeed: 100,
     loop: true
